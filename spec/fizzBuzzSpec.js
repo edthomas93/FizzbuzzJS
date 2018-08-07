61d0ac874d5e262020d08e9c0ccb684bf61ba993
edthomas93 <et1270@my.bristol.ac.uk>
2018-08-07 14:08:52 +0100

    refactor

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,21 +1,21 @@
 describe('FizzBuzz', function() {
+  beforeEach(function() {
+    fizzbuzz = new Fizzbuzz();
+  });
 
   var fizzbuzz;
 
   describe('knows when a number is', function() {
 
     it('divisible by 3', function() {
-      fizzbuzz = new Fizzbuzz();
       expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
     });
 
     it('divisible by 5', function() {
-      fizzbuzz = new Fizzbuzz();
       expect(fizzbuzz.isDivisibleByFive(90)).toBe(true);
     });
 
     it('divisible by 15', function() {
-      fizzbuzz = new Fizzbuzz();
       expect(fizzbuzz.isDivisibleByFifteen(30)).toBe(true);
     });
 
@@ -24,19 +24,16 @@ describe('FizzBuzz', function() {
   describe('knows when a number is NOT', function() {
 
     it('divisible by 3', function(){
-      fizzbuzz = new Fizzbuzz();
       expect(fizzbuzz.isDivisibleByThree(1)).toBe(false);
 
     });
 
     it('divisible by 5', function(){
-      fizzbuzz = new Fizzbuzz();
       expect(fizzbuzz.isDivisibleByFive(3)).toBe(false);
 
     });
 
     it('divisible by 15', function(){
-      fizzbuzz = new Fizzbuzz();
       expect(fizzbuzz.isDivisibleByFifteen(14)).toBe(false);
 
     });
